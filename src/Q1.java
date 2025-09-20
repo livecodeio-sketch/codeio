998d9f7120ba5d072cafa96b67b2105add0288d5
codeio <livecodeio@gmail.com>
2025-09-20 18:38:57 +0530

    week-2 Assignment

--- src/Q1.java
+++ src/Q1.java
@@ -1,5 +1,18 @@
 public class Q1 {
     public static void main(String[] args) {
-        System.out.println("Q1");
+        int n=3;
+        int star = 1;
+        for(int i=1; i<=n;i++){
+
+            for(int j=n; j>i; j--){
+                System.out.print(" ");
+            }
+
+            for(int k=1; k<=star; k++){
+                System.out.print("*");
+            }
+            star += 2;
+            System.out.println();
+        }
     }
 }
